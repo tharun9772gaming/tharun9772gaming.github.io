032c93e466432ae6cc40e26188c133f207112054
Tharun9772Gaming <tharun2015gamer@gmail.com>
2026-07-31 15:01:59 -0400

    Update direcr.js

--- Xbox Hub/games/direcr.js
+++ Xbox Hub/games/direcr.js
@@ -1,6 +1,6 @@
 (function() {
 const _0x4f22 = ["\x64\x65\x62\x75\x67", "\x69\x6E\x69\x74"];
-const UI_THEME_MODE_DEFAULT = 'light'; 
+const UI_THEME_MODE_DEFAULT = 'light';
 const UI_THEME_MODE_DARK = 'dark';
 const UI_THEME_MODE_AUTO = 'system';
 const UI_ELEMENT_MIN_WIDTH_PIXELS = 0;
